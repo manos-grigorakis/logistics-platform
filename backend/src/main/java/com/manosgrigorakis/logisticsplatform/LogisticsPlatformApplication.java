fe7289fa1bc357383b94155c9e2720331d2031af
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-11-04 07:12:30 +0200

    feat: initialize Spring Boot project and establish connection with database

---
+++ backend/src/main/java/com/manosgrigorakis/logisticsplatform/LogisticsPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.manosgrigorakis.logisticsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsPlatformApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/manosgrigorakis/logisticsplatform/LogisticsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manosgrigorakis.logisticsplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
