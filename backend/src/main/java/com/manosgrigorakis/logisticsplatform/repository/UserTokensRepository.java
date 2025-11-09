8f7b85fc58f9ce0b97c2cf3a90abed111039a24f
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-11-09 02:36:14 +0200

    feat: add scheduled job to clear expired tokens every day at 4:00

--- backend/src/main/java/com/manosgrigorakis/logisticsplatform/repository/UserTokensRepository.java
+++ backend/src/main/java/com/manosgrigorakis/logisticsplatform/repository/UserTokensRepository.java
@@ -4,9 +4,12 @@ import com.manosgrigorakis.logisticsplatform.model.UserTokens;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface UserTokensRepository extends JpaRepository<UserTokens, Long> {
     Optional<UserTokens> findByToken(String token);
+
+    void deleteAllByExpiresAtBefore(LocalDateTime current);
 }
