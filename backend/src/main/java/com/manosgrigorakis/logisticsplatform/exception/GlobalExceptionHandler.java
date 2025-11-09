c9bdb85d7bc00144ce9af1ea4d3e18b54b66caf6
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-11-09 15:17:31 +0200

    feat: add field validation exception

--- backend/src/main/java/com/manosgrigorakis/logisticsplatform/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/manosgrigorakis/logisticsplatform/exception/GlobalExceptionHandler.java
@@ -2,11 +2,34 @@ package com.manosgrigorakis.logisticsplatform.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    // Field validation error - 400
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleFieldValidationException(MethodArgumentNotValidException exc) {
+        Map<String, String> details = new HashMap<>();
+
+        exc.getBindingResult().getFieldErrors().forEach(error ->
+                details.put(error.getField(), error.getDefaultMessage())
+        );
+
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Field Validation Failed",
+                System.currentTimeMillis(),
+                details
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
     // Not found - 404
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException exc) {
