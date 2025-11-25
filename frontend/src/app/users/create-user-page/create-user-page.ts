ebb4537701307beccfa5b69313e10b929c3df300
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-11-25 17:52:55 +0200

    refactor: create user page and use instead user form component removing boilerplate code

--- frontend/src/app/users/create-user-page/create-user-page.ts
+++ frontend/src/app/users/create-user-page/create-user-page.ts
@@ -1,90 +1,25 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { RolesService } from '../../roles/roles.service';
-import { Role } from '../../roles/models/role';
-import { FormBuilder, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
-import { LoadingSpinner } from '../../shared/ui/loading-spinner/loading-spinner';
+import { Component, inject } from '@angular/core';
 import { UsersService } from '../users.service';
 import { CreateUserRequest } from '../models/create-user-request';
-import { PrimaryButton } from '../../shared/ui/primary-button/primary-button';
-import { MainInput } from '../../shared/forms/main-input/main-input';
 import { toast } from 'ngx-sonner';
 import { Router } from '@angular/router';
+import { UserForm } from '../user-form/user-form';
 
 @Component({
   selector: 'app-create-user-page',
-  imports: [ReactiveFormsModule, LoadingSpinner, PrimaryButton, MainInput],
+  imports: [UserForm],
   templateUrl: './create-user-page.html',
   styleUrl: './create-user-page.css',
 })
-export class CreateUserPage implements OnInit {
-  private rolesService: RolesService = inject(RolesService);
+export class CreateUserPage {
   private userService: UsersService = inject(UsersService);
-  private formBuilder: FormBuilder = inject(FormBuilder);
   private router = inject(Router);
-  public roles: Role[] = [];
   public isLoading: boolean = false;
   public errorMessage?: string;
 
-  userForm = this.formBuilder.group({
-    firstName: new FormControl<string>('', {
-      nonNullable: true,
-      validators: Validators.required,
-    }),
-    lastName: new FormControl('', { nonNullable: true, validators: Validators.required }),
-    email: new FormControl<string>('', {
-      nonNullable: true,
-      validators: [Validators.required, Validators.email],
-    }),
-    phone: new FormControl<string>(''),
-    roleId: new FormControl<number | null>(null, {
-      validators: Validators.required,
-    }),
-  });
-
-  ngOnInit(): void {
-    this.rolesService.fetchRoles().subscribe({
-      next: (res) => {
-        this.roles = res;
-      },
-      error: (err) => {
-        if (err.status === 500) {
-          this.errorMessage = 'Server error. Please try again later';
-        } else {
-          this.errorMessage = 'An error occured. Please try again';
-        }
-      },
-    });
-  }
-
-  get email(): FormControl {
-    return this.userForm.get('email') as FormControl;
-  }
-
-  get roleId(): FormControl {
-    return this.userForm.get('roleId') as FormControl;
-  }
-
-  public onSubmit(): void {
-    if (this.userForm.invalid) {
-      // Mark all controls as touched & re-run validators
-      Object.values(this.userForm.controls).forEach((control) => {
-        control.markAsTouched();
-        control.updateValueAndValidity({ emitEvent: true });
-      });
-      return;
-    }
-
+  public onSubmit(data: CreateUserRequest): void {
+    this.errorMessage = undefined;
     this.isLoading = true;
-
-    const { firstName, lastName, email, phone, roleId } = this.userForm.getRawValue();
-
-    const data: CreateUserRequest = {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      phone: phone?.trim() === '' ? null : phone,
-      roleId: roleId!,
-    };
 
     this.userService.createUser(data).subscribe({
       next: (res) => {
@@ -95,8 +30,7 @@ export class CreateUserPage implements OnInit {
       error: (err) => {
         this.isLoading = false;
         if (err.status === 409) {
-          this.email.reset();
-          this.errorMessage = `User already exists with email ${email}`;
+          this.errorMessage = `User already exists with email ${data.email}`;
         } else if (err.status === 500) {
           this.errorMessage = 'Server error. Please try again later';
         } else {
