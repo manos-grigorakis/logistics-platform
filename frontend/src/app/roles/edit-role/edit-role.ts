7aa259469f08ef4a3fc601fc1409b1b6d6455814
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-11-28 14:08:44 +0200

    feat: add edit role business logic using role-form component

--- frontend/src/app/roles/edit-role/edit-role.ts
+++ frontend/src/app/roles/edit-role/edit-role.ts
@@ -1,11 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { RolesService } from '../roles.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Role } from '../models/role';
+import { RoleRequest } from '../models/role-request';
+import { toast } from 'ngx-sonner';
+import { RoleForm } from '../role-form/role-form';
 
 @Component({
   selector: 'app-edit-role',
-  imports: [],
+  imports: [RoleForm],
   templateUrl: './edit-role.html',
   styleUrl: './edit-role.css',
 })
-export class EditRole {
+export class EditRole implements OnInit {
+  private rolesService: RolesService = inject(RolesService);
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  private router: Router = inject(Router);
+  private id: number = 0;
 
+  public isLoading: boolean = false;
+  public errorMessage?: string;
+  public role?: Role;
+
+  ngOnInit(): void {
+    let tempId = this.route.snapshot.paramMap.get('id');
+
+    if (!tempId) {
+      toast.error('Invalid role id');
+      this.router.navigate(['/roles']);
+      return;
+    }
+
+    this.id = parseInt(tempId);
+    this.fetchRoleById(this.id);
+  }
+
+  public onSubmit(data: RoleRequest): void {
+    this.isLoading = true;
+    this.errorMessage = undefined;
+
+    this.rolesService.updateRole(this.id, data).subscribe({
+      next: (res) => {
+        this.isLoading = false;
+        this.router.navigate(['/roles']);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        if (err.status === 500) {
+          this.errorMessage = 'Server error. Please try again later';
+        } else {
+          this.errorMessage = 'An error occured. Please try again';
+        }
+      },
+    });
+  }
+
+  private fetchRoleById(id: number): void {
+    this.isLoading = true;
+
+    this.rolesService.fetchRole(id).subscribe({
+      next: (res) => {
+        this.isLoading = false;
+        this.role = res;
+        this.errorMessage = undefined;
+      },
+      error: (err) => {
+        this.isLoading = false;
+
+        if (err.status === 404) {
+          this.errorMessage = 'Role not found';
+        } else if (err.status === 500) {
+          this.errorMessage = 'Server error. Please try again';
+        } else {
+          this.errorMessage = 'An error has occured. Please try again';
+        }
+      },
+    });
+  }
 }
--- frontend/src/app/roles/roles.service.ts
+++ frontend/src/app/roles/roles.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { AuthService } from '../auth/services/auth.service';
 import { environment } from '../../environments/environment';
 import { Role } from './models/role';
+import { RoleRequest } from './models/role-request';
 
 @Injectable({
   providedIn: 'root',
@@ -20,6 +21,20 @@ export class RolesService {
     const headers = { Authorization: `Bearer ${this.jwtToken}` };
 
     return this.http.get<Role[]>(`${environment.apiUrl}/roles`, { headers: headers });
+  }
+
+  public fetchRole(id: number): Observable<Role> {
+    this.jwtToken = this.authService.getJwtToken();
+    const headers = { Authorization: `Bearer ${this.jwtToken}` };
+
+    return this.http.get<Role>(`${environment.apiUrl}/roles/${id}`, { headers: headers });
+  }
+
+  public updateRole(id: number, data: RoleRequest): Observable<Role> {
+    this.jwtToken = this.authService.getJwtToken();
+    const headers = { Authorization: `Bearer ${this.jwtToken}` };
+
+    return this.http.put<Role>(`${environment.apiUrl}/roles/${id}`, data, { headers: headers });
   }
 
   public deleteRole(id: number): Observable<void> {
