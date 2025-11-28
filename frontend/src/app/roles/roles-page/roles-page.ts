30373428f46dae3b3fef616b40d0b196038947ce
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-11-28 11:28:16 +0200

    feat: add roles table and update side navigation bar link

--- frontend/src/app/roles/roles-page/roles-page.ts
+++ frontend/src/app/roles/roles-page/roles-page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RolesTable } from '../roles-table/roles-table';
+import { Role } from '../models/role';
+import { RolesService } from '../roles.service';
 
 @Component({
   selector: 'app-roles-page',
@@ -7,4 +9,32 @@ import { RolesTable } from '../roles-table/roles-table';
   templateUrl: './roles-page.html',
   styleUrl: './roles-page.css',
 })
-export class RolesPage {}
+export class RolesPage implements OnInit {
+  private rolesService: RolesService = inject(RolesService);
+
+  public isLoading: boolean = false;
+  public roles: Role[] = [];
+  public errorMessage: string = '';
+
+  ngOnInit(): void {
+    this.fetchRoles();
+  }
+
+  private fetchRoles(): void {
+    this.isLoading = true;
+    this.rolesService.fetchRoles().subscribe({
+      next: (res) => {
+        this.isLoading = false;
+        this.roles = res;
+      },
+      error: (err) => {
+        this.isLoading = false;
+        if (err.status === 500) {
+          this.errorMessage = 'Server error. Please try again later';
+        } else {
+          this.errorMessage = 'An error occured. Please try again';
+        }
+      },
+    });
+  }
+}
--- frontend/src/app/roles/roles-table/roles-table.ts
+++ frontend/src/app/roles/roles-table/roles-table.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Role } from '../models/role';
+import { LoadingSpinner } from '../../shared/ui/loading-spinner/loading-spinner';
 
 @Component({
   selector: 'app-roles-table',
-  imports: [],
+  imports: [RouterLink, LoadingSpinner],
   templateUrl: './roles-table.html',
   styleUrl: './roles-table.css',
 })
 export class RolesTable {
+  @Input() isLoading?: boolean;
+  @Input() roles?: Role[];
+  @Output() deleteRole = new EventEmitter<number>();
 
+  public onDeleteRole(id: number): void {
+    this.deleteRole.emit(id);
+  }
 }
