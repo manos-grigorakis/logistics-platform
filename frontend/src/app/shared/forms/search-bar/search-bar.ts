ff78f9e225d3390edd5c243ee903a31bd8192a3c
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-11-28 07:42:39 +0200

    feat: update search bar component to emmit the input via event

--- frontend/src/app/shared/forms/search-bar/search-bar.ts
+++ frontend/src/app/shared/forms/search-bar/search-bar.ts
@@ -1,12 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [NgIcon],
+  imports: [NgIcon, FormsModule],
   templateUrl: './search-bar.html',
   styleUrl: './search-bar.css',
 })
 export class SearchBar {
   @Input() placeholder: string = 'Search...';
+  @Input() value: string = '';
+  @Output() valueChange = new EventEmitter<string>();
+
+  public onInputChange(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    this.valueChange.emit(value);
+  }
 }
