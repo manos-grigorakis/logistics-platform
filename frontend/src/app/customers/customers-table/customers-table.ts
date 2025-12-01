364ad829bc1868763b22fba12305bbe6789d04da
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-12-02 00:29:38 +0200

    feat: add pipe to transform customer type in table and create pills

--- frontend/src/app/customers/customers-table/customers-table.ts
+++ frontend/src/app/customers/customers-table/customers-table.ts
@@ -3,10 +3,11 @@ import { Customer } from '../models/customer';
 import { RouterLink } from '@angular/router';
 import { LoadingSpinner } from '../../shared/ui/loading-spinner/loading-spinner';
 import { NgClass } from '@angular/common';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-customers-table',
-  imports: [RouterLink, LoadingSpinner, NgClass],
+  imports: [RouterLink, LoadingSpinner, NgClass, TitleCasePipe],
   templateUrl: './customers-table.html',
   styleUrl: './customers-table.css',
 })
@@ -24,5 +25,16 @@ export class CustomersTable {
   public onCheckboxClick(event: MouseEvent, customerId: number): void {
     event.stopPropagation();
     this.toggleCustomerSelection.emit(customerId);
+  }
+
+  public customerTypeBadgeColor(type: string): string {
+    switch (type) {
+      case 'COMPANY':
+        return 'bg-primary-100 text-primary-800';
+      case 'INDIVIDUAL':
+        return 'bg-success-light text-success-dark';
+      default:
+        return 'bg-secondary-100 text-secondary-800';
+    }
   }
 }
