ec59922e2019263194e5d892da938e845afda866
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-12-03 20:40:07 +0200

    feat: add AWS S3 SDK and configure its bean with minio

---
+++ backend/src/main/java/com/manosgrigorakis/logisticsplatform/config/S3ClientConfig.java
@@ -0,0 +1,43 @@
+package com.manosgrigorakis.logisticsplatform.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
+
+@Configuration
+public class S3ClientConfig {
+    @Value("${app.minio.accessKey}")
+    private String accessKey;
+
+    @Value("${app.minio.secretKey}")
+    private String secretKey;
+
+    @Value("${app.minio.endpoint}")
+    private String endpoint;
+
+    @Value("${app.aws.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        Region s3Region = Region.of(region);
+
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .region(s3Region)
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(true) // required for minio
+                        .build())
+                .build();
+    }
+}
