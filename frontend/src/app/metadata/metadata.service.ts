beecd37b7f2f84d27dc33cec1acd6837fc4c2ffe
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-12-10 03:35:07 +0200

    feat: add metadata fetch for quotes statuses and quotes item units with in-memory cach

--- frontend/src/app/metadata/metadata.service.ts
+++ frontend/src/app/metadata/metadata.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { AuthService } from '../auth/services/auth.service';
 import { environment } from '../../environments/environment';
 
@@ -8,6 +8,9 @@ import { environment } from '../../environments/environment';
   providedIn: 'root',
 })
 export class MetadataService {
+  public quoteStatuses$ = new BehaviorSubject<string[]>([]);
+  public quoteItemUnits$ = new BehaviorSubject<string[]>([]);
+
   private http = inject(HttpClient);
   private authService: AuthService = inject(AuthService);
   private jwtToken?: string | null;
@@ -19,5 +22,33 @@ export class MetadataService {
     return this.http.get<Array<string>>(`${environment.apiUrl}/metadata/customer-types`, {
       headers: headers,
     });
+  }
+
+  public fetchQuotesStatuses(): void {
+    this.jwtToken = this.authService.getJwtToken();
+    const headers = { Authorization: `Bearer ${this.jwtToken}` };
+
+    if (this.quoteStatuses$.value.length > 0) return;
+
+    this.http
+      .get<string[]>(`${environment.apiUrl}/metadata/quote-statuses`, { headers: headers })
+      .subscribe({
+        next: (res) => this.quoteStatuses$.next(res),
+        error: (err) => console.error('Failed to fetch quote statuses', err),
+      });
+  }
+
+  public fetchQuoteItemUnits(): void {
+    this.jwtToken = this.authService.getJwtToken();
+    const headers = { Authorization: `Bearer ${this.jwtToken}` };
+
+    if (this.quoteItemUnits$.value.length > 0) return;
+
+    this.http
+      .get<string[]>(`${environment.apiUrl}/metadata/quote-item-units`, { headers: headers })
+      .subscribe({
+        next: (res) => this.quoteItemUnits$.next(res),
+        error: (err) => console.error('Failed to fetch quote units', err),
+      });
   }
 }
