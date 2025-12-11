19dcc819593559ac885e301e7ecee04af7a82dc1
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-12-11 04:47:10 +0200

    style: update quotes table with badges for statuses

--- frontend/src/app/quotes/quotes-table/quotes-table.ts
+++ frontend/src/app/quotes/quotes-table/quotes-table.ts
@@ -3,11 +3,11 @@ import { LoadingSpinner } from '../../shared/ui/loading-spinner/loading-spinner'
 import { CurrencyPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { QuotesListItem } from '../models/quotes-list-item';
-import { DatePipe } from '@angular/common';
+import { NgClass, DatePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-quotes-table',
-  imports: [LoadingSpinner, CurrencyPipe, RouterLink, DatePipe],
+  imports: [LoadingSpinner, CurrencyPipe, RouterLink, DatePipe, TitleCasePipe, NgClass],
   templateUrl: './quotes-table.html',
   styleUrl: './quotes-table.css',
 })
@@ -19,5 +19,20 @@ export class QuotesTable {
 
   public onViewQuoteClick(id: number): void {
     this.onViewQuote.emit(id);
+  }
+
+  public quoteStatusBadgeColor(status: string): string {
+    switch (status) {
+      case 'DRAFT':
+        return 'bg-secondary-100 text-secondary-800';
+      case 'SENT':
+        return 'bg-primary-100 text-primary-800';
+      case 'ACCEPTED':
+        return 'bg-success-light text-success-dark';
+      case 'EXPIRED':
+        return 'bg-warning-light text-warning-dark';
+      default:
+        return 'bg-slate-200 text-slate-700';
+    }
   }
 }
