a7dd832aac3f1283488d8323e3b7efea04447654
manos <manos.grigorakis03@gmail.com>
2025-12-14 05:57:44 +0200

    chore: add production seeders and schema

--- backend/src/main/resources/db/seeders/seed.prod.sql
+++ backend/src/main/resources/db/seeders/seed.prod.sql
@@ -1,8 +1,8 @@
 INSERT INTO roles (name, description, is_editable, created_at)
 VALUES
-    ('ADMIN', 'Full system access. Manages users, roles, settings and critical operations.', FALSE,NOW()),
-    ('MANAGER', 'Oversees daily operations, assigns jobs and monitors overall progress.', FALSE,NOW()),
-    ('EMPLOYEE', 'Handles assigned tasks and updates job or customer information.', FALSE,NOW());
+    ('ADMIN', 'Full system access. Manages users, roles, settings and critical operations.', 0 ,NOW()),
+    ('MANAGER', 'Oversees daily operations, assigns jobs and monitors overall progress.', 0 ,NOW()),
+    ('EMPLOYEE', 'Handles assigned tasks and updates job or customer information.', 0 ,NOW());
 
 -- Encrypt Password for Initial Setup: https://www.bcryptcalculator.com/
 -- INSERT INTO users(role_id, first_name, last_name, email, password, phone, status, enabled, created_at)
