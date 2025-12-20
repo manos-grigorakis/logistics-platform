79e0b1021284e9fd46a17a55868d51ee75c1c76a
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-12-20 18:03:55 +0200

    chore: add commnets on methods and re-arrange variables and methods into logical groups

--- frontend/src/app/customers/view-customer/customer-tabs/customer-tab-quotes/customer-tab-quotes.ts
+++ frontend/src/app/customers/view-customer/customer-tabs/customer-tab-quotes/customer-tab-quotes.ts
@@ -33,53 +33,53 @@ import { QuotesPerCustomerParameters } from '../../../models/quotes-per-customer
   styleUrl: './customer-tab-quotes.css',
 })
 export class CustomerTabQuotes implements OnInit, OnDestroy {
+  // Quotes
   public isQuotesLoading: boolean = false;
-  public isQuotePdfLoading: boolean = false;
   public quotes: QuotePerCustomer[] = [];
   public errorMessage?: string = undefined;
-  public quotesErrorMessage?: string = undefined;
   public quotesStatuses: string[] = [];
   public finalizedStatuses: string[] = ['accepted', 'rejected', 'cancelled', 'expired'];
 
   // PDF Modal
   public isModalOpen: boolean = false;
   public pdfUrl?: string = undefined;
+  public isQuotePdfLoading: boolean = false;
+  public quotesErrorMessage?: string = undefined;
 
   // Pagination
   public currentPage: number = 0;
   public totalPages: number = 0;
   public totalElements: number = 0;
-  public isFirstPage: boolean = false;
   private quotesToShow: number = 5;
   public pageSize: number = this.quotesToShow;
 
-  // Quotes Status Modal
+  // Quotes Status Update Modal
   public isQuotesStatusModalEnabled: boolean = false;
   public quoteStatusModalHeader?: string = undefined;
   public quoteStatusModalMessage?: string = undefined;
-
-  // Filters
-  public activeSortLabel: string = 'Sort by';
-  public activeFilterLabel: string = 'Filter by';
-  private searchChanged$ = new Subject<string>();
-  private subSearch$?: Subscription;
-  private currentParams: QuotesPerCustomerParameters = {
-    page: 0,
-  };
-
-  private customersService = inject(CustomersService);
-  private quotesService = inject(QuotesService);
-  private metadataService = inject(MetadataService);
-
-  private sub$?: Subscription;
-  private quotesStatuses$?: Subscription;
-  private quotesReqSub$?: Subscription;
-
   public editingQuoteId?: number;
   public pendingStatus?: string;
   private quoteToBeUpdated?: QuotePerCustomer = undefined;
   private desiredQuoteStatus?: string = undefined;
 
+  // Filters
+  public activeSortLabel: string = 'Sort by';
+  public activeFilterLabel: string = 'Filter by';
+  private searchChanged$ = new Subject<string>();
+  private currentParams: QuotesPerCustomerParameters = { page: 0 };
+
+  // Services
+  private customersService = inject(CustomersService);
+  private quotesService = inject(QuotesService);
+  private metadataService = inject(MetadataService);
+
+  // Subscriptions
+  private sub$?: Subscription;
+  private quotesStatuses$?: Subscription;
+  private quotesReqSub$?: Subscription;
+  private subSearch$?: Subscription;
+
+  // Lifecycle
   ngOnInit(): void {
     this.fetchQuotesPerCustomer({ size: this.quotesToShow });
     this.metadataService.fetchQuotesStatuses();
@@ -102,11 +102,18 @@ export class CustomerTabQuotes implements OnInit, OnDestroy {
     this.searchChanged$.next(value);
   }
 
+  /**
+   * Opens the selected quote in PDF modal
+   * @param id quote id
+   */
   public openQuote(id: number): void {
     this.fetchQuote(id);
     this.isModalOpen = true;
   }
 
+  /**
+   * Close PDF modal on event
+   */
   public closeQuote(): void {
     this.isModalOpen = false;
   }
@@ -118,30 +125,41 @@ export class CustomerTabQuotes implements OnInit, OnDestroy {
     this.fetchQuotesPerCustomer({ page: page, size: this.quotesToShow });
   }
 
+  /**
+   * Applies CSS badge for quote status based on the status
+   * @param status quote status
+   * @returns applies CSS badge style for status
+   */
   public quoteStatusBadgeColor(status: string): string {
     return quoteStatusBadgeColor(status);
   }
 
+  /**
+   * Updates the quote status on confirm click event
+   * @returns
+   */
   public onQuotesStatusModalConfirmClick(): void {
     if (!this.quoteToBeUpdated || !this.desiredQuoteStatus) return;
 
-    // Close modal
     this.isQuotesStatusModalEnabled = false;
-
-    // Update
     this.onStatusChange(this.quoteToBeUpdated, this.desiredQuoteStatus);
-
-    // Reset
     this.resetQuoteStatusModalVariables();
   }
 
+  /**
+   * Close alert modal on cancel event,
+   * when updating the quote status
+   */
   public onQuotesStatusModalCancelClick(): void {
     this.isQuotesStatusModalEnabled = false;
-
-    // Reset
     this.resetQuoteStatusModalVariables();
   }
 
+  /**
+   * Build and setup the alert modal before updating the quote status
+   * @param quote selected quote
+   * @param newStatus desired status to be updated
+   */
   public handleQuotesStatusModal(quote: QuotePerCustomer, newStatus: string): void {
     this.quoteToBeUpdated = quote;
     this.desiredQuoteStatus = newStatus;
@@ -157,7 +175,163 @@ export class CustomerTabQuotes implements OnInit, OnDestroy {
     this.isQuotesStatusModalEnabled = true;
   }
 
-  public onStatusChange(quote: QuotePerCustomer, newStatus?: string): void {
+  /**
+   * Refresh the quotes on refresh event
+   */
+  public onRefresh(): void {
+    this.fetchQuotesPerCustomer({ size: this.quotesToShow });
+  }
+
+  /**
+   * Sorts the quotes based on the selected query,
+   * and updates the label to match the active query
+   * @param query selected option to sort the data
+   * @returns
+   */
+  public onSort(query: string) {
+    if (!query) return;
+
+    switch (query) {
+      case 'sort-all':
+        this.activeSortLabel = 'Sort by';
+        this.fetchQuotesPerCustomer({ page: 0, sortBy: undefined, sortDirection: undefined });
+        break;
+      case 'sort-asc-by-number':
+        this.activeSortLabel = 'Number 0 → 9';
+        this.fetchQuotesPerCustomer({ page: 0, sortBy: 'number', sortDirection: 'asc' });
+        break;
+      case 'sort-desc-by-number':
+        this.activeSortLabel = 'Number 9 → 0';
+        this.fetchQuotesPerCustomer({ page: 0, sortBy: 'number', sortDirection: 'desc' });
+        break;
+      case 'sort-asc-by-issue-date':
+        this.activeSortLabel = 'Date 0 → 9';
+        this.fetchQuotesPerCustomer({ page: 0, sortBy: 'issueDate', sortDirection: 'asc' });
+        break;
+      case 'sort-desc-by-issue-date':
+        this.activeSortLabel = 'Date 9 → 0';
+        this.fetchQuotesPerCustomer({ page: 0, sortBy: 'issueDate', sortDirection: 'desc' });
+        break;
+    }
+  }
+
+  /**
+   * Filters quotes based on the query,
+   * and updates the label to match the active query
+   * @param query selected option to filter quotes
+   * @returns
+   */
+  public onFilter(query: string) {
+    if (!query) return;
+
+    switch (query) {
+      case 'filter-by-all':
+        this.activeFilterLabel = 'Filter by';
+        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: undefined, sortDirection: undefined });
+        break;
+      case 'filter-by-quote-status-draft':
+        this.activeFilterLabel = 'Draft';
+        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: 'DRAFT' });
+        break;
+      case 'filter-by-quote-status-sent':
+        this.activeFilterLabel = 'Sent';
+        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: 'SENT' });
+        break;
+      case 'filter-by-quote-status-accepted':
+        this.activeFilterLabel = 'Accepted';
+        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: 'ACCEPTED' });
+        break;
+      case 'filter-by-quote-status-rejected':
+        this.activeFilterLabel = 'Rejected';
+        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: 'REJECTED' });
+        break;
+      case 'filter-by-quote-status-expired':
+        this.activeFilterLabel = 'Expired';
+        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: 'EXPIRED' });
+        break;
+      case 'filter-by-quote-status-cancelled':
+        this.activeFilterLabel = 'Cancelled';
+        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: 'CANCELLED' });
+        break;
+    }
+  }
+
+  /**
+   * Gets all the quotes for the selected customer
+   * Applies filtering and sorting if exist
+   * @param params params to apply filtering & sorting
+   */
+  private fetchQuotesPerCustomer(params?: QuotesPerCustomerParameters): void {
+    this.sub$?.unsubscribe();
+
+    const finalParams: QuotesPerCustomerParameters = {
+      ...this.currentParams,
+      ...params,
+    };
+
+    // Saved for future requests
+    this.currentParams = finalParams;
+
+    this.sub$ = this.customersService.selectedCustomer$.subscribe((c) => {
+      if (!c) return;
+      this.isQuotesLoading = true;
+      this.errorMessage = undefined;
+      this.quotesReqSub$?.unsubscribe();
+
+      this.quotesReqSub$ = this.customersService.quotesPerCustomer(c.id, finalParams).subscribe({
+        next: (res) => {
+          this.isQuotesLoading = false;
+          this.quotes = res.content;
+
+          this.currentPage = res.number;
+          this.totalPages = res.totalPages;
+          this.totalElements = res.totalElements;
+        },
+        error: (err) => {
+          this.isQuotesLoading = false;
+          if (err.status === 500) {
+            this.errorMessage = 'Server error. Please try again';
+          } else {
+            this.errorMessage = 'An error occured. Please try again';
+          }
+        },
+      });
+    });
+  }
+
+  /**
+   * Fetch a quote by its id
+   * @param id quote id
+   */
+  private fetchQuote(id: number): void {
+    this.isQuotePdfLoading = true;
+    this.quotesErrorMessage = undefined;
+
+    this.quotesService.fetchQuoteById(id).subscribe({
+      next: (res) => {
+        this.isQuotePdfLoading = false;
+        this.pdfUrl = res.pdfUrl;
+      },
+      error: (err) => {
+        this.isQuotePdfLoading = false;
+        if (err.status === 404) {
+          this.quotesErrorMessage = `Quote with id ${id} not exist`;
+        } else if (err.status === 500) {
+          this.quotesErrorMessage = 'Server error. Please try again';
+        } else {
+          this.quotesErrorMessage = 'An error occured. Please try again later';
+        }
+      },
+    });
+  }
+
+  /**
+   * Updates the selected quote status to the desired status
+   * @param quote selected quote
+   * @param newStatus desired status of the selected quote
+   * @returns
+   */
+  private onStatusChange(quote: QuotePerCustomer, newStatus?: string): void {
     if (!newStatus) return;
     const normalized = newStatus.toLowerCase();
 
@@ -201,132 +375,10 @@ export class CustomerTabQuotes implements OnInit, OnDestroy {
     });
   }
 
-  public onRefresh(): void {
-    this.fetchQuotesPerCustomer({ size: this.quotesToShow });
-  }
-
-  public onSort(query: string) {
-    if (!query) return;
-
-    switch (query) {
-      case 'sort-all':
-        this.activeSortLabel = 'Sort by';
-        this.fetchQuotesPerCustomer({ page: 0, sortBy: undefined, sortDirection: undefined });
-        break;
-      case 'sort-asc-by-number':
-        this.activeSortLabel = 'Number 0 → 9';
-        this.fetchQuotesPerCustomer({ page: 0, sortBy: 'number', sortDirection: 'asc' });
-        break;
-      case 'sort-desc-by-number':
-        this.activeSortLabel = 'Number 9 → 0';
-        this.fetchQuotesPerCustomer({ page: 0, sortBy: 'number', sortDirection: 'desc' });
-        break;
-      case 'sort-asc-by-issue-date':
-        this.activeSortLabel = 'Date 0 → 9';
-        this.fetchQuotesPerCustomer({ page: 0, sortBy: 'issueDate', sortDirection: 'asc' });
-        break;
-      case 'sort-desc-by-issue-date':
-        this.activeSortLabel = 'Date 9 → 0';
-        this.fetchQuotesPerCustomer({ page: 0, sortBy: 'issueDate', sortDirection: 'desc' });
-        break;
-    }
-  }
-
-  public onFilter(query: string) {
-    if (!query) return;
-
-    switch (query) {
-      case 'filter-by-all':
-        this.activeFilterLabel = 'Filter by';
-        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: undefined, sortDirection: undefined });
-        break;
-      case 'filter-by-quote-status-draft':
-        this.activeFilterLabel = 'Draft';
-        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: 'DRAFT' });
-        break;
-      case 'filter-by-quote-status-sent':
-        this.activeFilterLabel = 'Sent';
-        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: 'SENT' });
-        break;
-      case 'filter-by-quote-status-accepted':
-        this.activeFilterLabel = 'Accepted';
-        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: 'ACCEPTED' });
-        break;
-      case 'filter-by-quote-status-rejected':
-        this.activeFilterLabel = 'Rejected';
-        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: 'REJECTED' });
-        break;
-      case 'filter-by-quote-status-expired':
-        this.activeFilterLabel = 'Expired';
-        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: 'EXPIRED' });
-        break;
-      case 'filter-by-quote-status-cancelled':
-        this.activeFilterLabel = 'Cancelled';
-        this.fetchQuotesPerCustomer({ page: 0, quoteStatus: 'CANCELLED' });
-        break;
-    }
-  }
-
-  private fetchQuotesPerCustomer(params?: QuotesPerCustomerParameters): void {
-    this.sub$?.unsubscribe();
-
-    const finalParams: QuotesPerCustomerParameters = {
-      ...this.currentParams,
-      ...params,
-    };
-
-    // Saved for future requests
-    this.currentParams = finalParams;
-
-    this.sub$ = this.customersService.selectedCustomer$.subscribe((c) => {
-      if (!c) return;
-      this.isQuotesLoading = true;
-      this.errorMessage = undefined;
-      this.quotesReqSub$?.unsubscribe();
-
-      this.quotesReqSub$ = this.customersService.quotesPerCustomer(c.id, finalParams).subscribe({
-        next: (res) => {
-          this.isQuotesLoading = false;
-          this.quotes = res.content;
-
-          this.currentPage = res.number;
-          this.totalPages = res.totalPages;
-          this.totalElements = res.totalElements;
-        },
-        error: (err) => {
-          this.isQuotesLoading = false;
-          if (err.status === 500) {
-            this.errorMessage = 'Server error. Please try again';
-          } else {
-            this.errorMessage = 'An error occured. Please try again';
-          }
-        },
-      });
-    });
-  }
-
-  private fetchQuote(id: number): void {
-    this.isQuotePdfLoading = true;
-    this.quotesErrorMessage = undefined;
-
-    this.quotesService.fetchQuoteById(id).subscribe({
-      next: (res) => {
-        this.isQuotePdfLoading = false;
-        this.pdfUrl = res.pdfUrl;
-      },
-      error: (err) => {
-        this.isQuotePdfLoading = false;
-        if (err.status === 404) {
-          this.quotesErrorMessage = `Quote with id ${id} not exist`;
-        } else if (err.status === 500) {
-          this.quotesErrorMessage = 'Server error. Please try again';
-        } else {
-          this.quotesErrorMessage = 'An error occured. Please try again later';
-        }
-      },
-    });
-  }
-
+  /**
+   * Gets the quote statuses from the metadata service,
+   * excluding EXPIRED status
+   */
   private subscribeToQuotesStatuses(): void {
     this.quotesStatuses$ = this.metadataService.quoteStatuses$
       .pipe(
@@ -338,6 +390,9 @@ export class CustomerTabQuotes implements OnInit, OnDestroy {
       });
   }
 
+  /**
+   * Resets variables of the current selected quote
+   */
   private resetQuoteStatusModalVariables(): void {
     this.quoteToBeUpdated = undefined;
     this.desiredQuoteStatus = undefined;
@@ -345,6 +400,12 @@ export class CustomerTabQuotes implements OnInit, OnDestroy {
     this.pendingStatus = undefined;
   }
 
+  /**
+   * Handles search input changes and filters by quotes number
+   * Reset active filters & sorting when searching
+   * @param value search input
+   * @returns
+   */
   private onSearch(value: string): void {
     let param = value.trim();
 
