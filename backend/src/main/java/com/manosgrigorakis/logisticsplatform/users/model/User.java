239e60ddd86a882d460f5746bc2ae5b20b9b91a0
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-12-21 13:38:43 +0200

    feat: add method on user model to return full name

--- backend/src/main/java/com/manosgrigorakis/logisticsplatform/users/model/User.java
+++ backend/src/main/java/com/manosgrigorakis/logisticsplatform/users/model/User.java
@@ -75,4 +75,8 @@ public class User {
     public void onUpdate() {
         this.updatedAt = LocalDateTime.now();
     }
+
+    public String fullName() {
+        return firstName + " " + lastName;
+    }
 }
