4d163f5d3cd1074a388ce9ca12018364762a7024
Manos Grigorakis <manos.grigorakis03@gmail.com>
2026-03-11 23:43:13 +0200

    refactor: Resource not found exception to include error code too

--- backend/src/main/java/com/manosgrigorakis/logisticsplatform/common/exception/ResourceNotFoundException.java
+++ backend/src/main/java/com/manosgrigorakis/logisticsplatform/common/exception/ResourceNotFoundException.java
@@ -1,6 +1,11 @@
 package com.manosgrigorakis.logisticsplatform.common.exception;
 
+import lombok.Getter;
+
+@Getter
 public class ResourceNotFoundException extends RuntimeException {
+    private String errorCode = "";
+
     public ResourceNotFoundException(String message) {
         super(message);
     }
@@ -11,5 +16,10 @@ public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(Throwable cause) {
         super(cause);
+    }
+
+    public ResourceNotFoundException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode != null ? errorCode : "";
     }
 }
