cf3b1b14158e124eb856619abf62936d3832aae2
Manos Grigorakis <manos.grigorakis03@gmail.com>
2026-03-15 17:05:50 +0200

    refactor: BadRequest Exception to display errorCode

--- backend/src/main/java/com/manosgrigorakis/logisticsplatform/common/exception/BadRequestException.java
+++ backend/src/main/java/com/manosgrigorakis/logisticsplatform/common/exception/BadRequestException.java
@@ -7,19 +7,31 @@ import java.util.Map;
 @Getter
 public class BadRequestException extends RuntimeException {
     private final Map<String, Object> details;
+    private final String errorCode;
 
     public BadRequestException(String message) {
         super(message);
         this.details = Map.of();
+        this.errorCode = "";
+    }
+
+    public BadRequestException(String message, String errorCode, Map<String, Object> details) {
+        super(message);
+        this.errorCode = errorCode != null ? errorCode :  "";
+        this.details = details != null ? Map.copyOf(details) : Map.of();
     }
 
     public BadRequestException(String message, Map<String, Object> details) {
         super(message);
-
-        if(details != null) {
-            this.details = Map.copyOf(details);
-        } else {
-            this.details = Map.of();
-        }
+        this.details = details != null ? Map.copyOf(details) : Map.of();
+        this.errorCode = "";
     }
+
+    public BadRequestException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode != null ? errorCode :  "";
+        this.details = Map.of();
+    }
+
+
 }
