686a6dde56b0c30bde7b3030d0a66a6ce0fe83cd
Manos Grigorakis <manos.grigorakis03@gmail.com>
2026-03-27 12:59:33 +0200

    feat: add form submission for reconciliation process with proper error handling

--- frontend/src/app/payments/payments-page/payments-page.ts
+++ frontend/src/app/payments/payments-page/payments-page.ts
@@ -3,14 +3,25 @@ import { FileDropzone } from '../../shared/ui/file-dropzone/file-dropzone';
 import { CustomersService } from '../../customers/customers.service';
 import { Customer } from '../../customers/models/customer';
 import { NgSelectComponent } from '@ng-select/ng-select';
-import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
 import { PrimaryButton } from '../../shared/ui/primary-button/primary-button';
 import { NgIcon } from '@ng-icons/core';
 import { DetailedStepper } from '../../shared/ui/detailed-stepper/detailed-stepper';
+import { FormBuilder, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { PaymentsService } from '../payments.service';
+import { ReconciliationProcessRequest } from '../models/reconciliaton-process-request';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-payments-page',
-  imports: [FileDropzone, NgSelectComponent, PrimaryButton, NgIcon, DetailedStepper],
+  imports: [
+    FileDropzone,
+    NgSelectComponent,
+    PrimaryButton,
+    NgIcon,
+    DetailedStepper,
+    ReactiveFormsModule,
+  ],
   templateUrl: './payments-page.html',
   styleUrl: './payments-page.css',
 })
@@ -28,16 +39,93 @@ export class PaymentsPage implements OnInit {
   public customersList: Customer[] = [];
   public customersLoading: boolean = false;
   public customersErrorMessage?: string = undefined;
-  public customerSearch$!: Subject<string>;
+  public customerSearch$: Subject<string> = new Subject<string>();
 
   private customersService = inject(CustomersService);
+  private paymentService = inject(PaymentsService);
+
+  public isFormValid: boolean = false;
+  private formBuilder = inject(FormBuilder);
+
+  reconciliationForm = this.formBuilder.group({
+    customerId: new FormControl<number | null>(null, Validators.required),
+    invoicesFile: new FormControl<File | null>(null, Validators.required),
+    bankStatementFile: new FormControl<File | null>(null, Validators.required),
+  });
 
   ngOnInit(): void {
+    // Debouncer
+    this.customerSearch$.pipe(debounceTime(300), distinctUntilChanged()).subscribe((name) => {
+      this.fetchCustomers(name);
+    });
+
     this.fetchCustomers('');
   }
 
   public onClickFocusCustomersSelect(): void {
     this.customerSelect.focus();
+  }
+
+  public onInvoicesFileSelected(file: File): void {
+    this.invoicesFile.setValue(file);
+  }
+
+  public onBankFileSelected(file: File): void {
+    this.bankStatementFile.setValue(file);
+  }
+
+  // Getters
+  public get customerId(): FormControl {
+    return this.reconciliationForm.get('customerId') as FormControl;
+  }
+
+  public get invoicesFile(): FormControl {
+    return this.reconciliationForm.get('invoicesFile') as FormControl;
+  }
+
+  public get bankStatementFile(): FormControl {
+    return this.reconciliationForm.get('bankStatementFile') as FormControl;
+  }
+
+  public onFormSubmit(): void {
+    if (this.reconciliationForm.invalid) {
+      this.reconciliationForm.markAllAsTouched();
+      return;
+    }
+
+    const payload: ReconciliationProcessRequest = {
+      customerId: this.customerId.getRawValue(),
+      invoiceFile: this.invoicesFile.getRawValue(),
+      bankStatement: this.bankStatementFile.getRawValue(),
+    };
+
+    this.paymentService.reconciliationProcess(payload).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        let errorCode = err?.error?.errorCode;
+
+        switch (err.status) {
+          case 400:
+            toast.error('Failed to process the uploaded files. Please try again');
+            break;
+          case 404:
+            toast.error('Failed to process the uploaded files. Please try again');
+            break;
+          case 409:
+            if (errorCode === 'CUSTOMER_TIN_MISMATCH') {
+              toast.error("The selected customer's TIN number doesn't match the invoices file");
+            }
+            break;
+          case 500:
+            toast.error('Server error. Please try again');
+            break;
+          default:
+            toast.error('An error occurred. Please try again');
+        }
+      },
+    });
   }
 
   private fetchCustomers(customer: string): void {
