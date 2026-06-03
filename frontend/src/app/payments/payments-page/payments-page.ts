0f516844415b1cb95abaefa2f5099004b1590af7
manos <manos.grigorakis03@gmail.com>
2026-06-03 20:27:30 +0300

    refactor: payments to use api response

--- frontend/src/app/payments/payments-page/payments-page.ts
+++ frontend/src/app/payments/payments-page/payments-page.ts
@@ -92,13 +92,13 @@ export class PaymentsPage implements OnInit {
         setTimeout(() => {
           // Results step
           this.activeStep = 2;
-          this.results = res;
+          this.results = res.data;
         }, delay);
       },
       error: (err) => {
         this.activeStep = 0;
 
-        let errorCode = err?.error?.errorCode;
+        let errorCode = err?.error?.error?.errorCode;
 
         switch (err.status) {
           case 400:
