8ecbbb168be26aab416e47bebfb02dd44a6ace51
manos <manos.grigorakis03@gmail.com>
2026-06-03 21:26:55 +0300

    refactor: Controller level authentication tests with new API response wrapper

--- backend/src/test/java/com/manosgrigorakis/logisticsplatform/auth/TestResetPasswordResult.java
+++ backend/src/test/java/com/manosgrigorakis/logisticsplatform/auth/TestResetPasswordResult.java
@@ -1,8 +1,9 @@
 package com.manosgrigorakis.logisticsplatform.auth;
 
 import com.manosgrigorakis.logisticsplatform.auth.model.UserTokens;
+import com.manosgrigorakis.logisticsplatform.common.dto.ApiResponseWrapper;
 import com.manosgrigorakis.logisticsplatform.common.dto.MessageResponseDTO;
 import org.springframework.http.ResponseEntity;
 
-public record TestResetPasswordResult(ResponseEntity<MessageResponseDTO> response, UserTokens userTokens) {
+public record TestResetPasswordResult(ResponseEntity<ApiResponseWrapper<MessageResponseDTO>> response, UserTokens userTokens) {
 }
