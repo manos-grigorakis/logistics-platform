8c3ce2c87f63e2bb5278d4b633f86f8bbd26d82c
manos <manos.grigorakis03@gmail.com>
2026-06-04 18:57:18 +0300

    feat: translate not found page and forbidden page

--- frontend/src/app/forbidden-page/forbidden-page.ts
+++ frontend/src/app/forbidden-page/forbidden-page.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { PrimaryButton } from '../shared/ui/primary-button/primary-button';
 import { AuthService } from '../auth/services/auth.service';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-forbidden-page',
-  imports: [PrimaryButton],
+  imports: [PrimaryButton, TranslatePipe],
   templateUrl: './forbidden-page.html',
   styleUrl: './forbidden-page.css',
 })
