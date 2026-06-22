41b02da04954b379cd779720ab5f8fb744361378
manos <manos.grigorakis03@gmail.com>
2026-06-22 19:52:12 +0300

    refactor: company profile service by extracting find first company in db into a method

--- backend/src/main/java/com/manosgrigorakis/logisticsplatform/companyprofile/service/CompanyProfileService.java
+++ backend/src/main/java/com/manosgrigorakis/logisticsplatform/companyprofile/service/CompanyProfileService.java
@@ -3,8 +3,11 @@ package com.manosgrigorakis.logisticsplatform.companyprofile.service;
 import com.manosgrigorakis.logisticsplatform.companyprofile.dto.CompanyProfileCreateRequest;
 import com.manosgrigorakis.logisticsplatform.companyprofile.dto.CompanyProfileResponse;
 import com.manosgrigorakis.logisticsplatform.companyprofile.dto.CompanyProfileUpdateRequest;
+import com.manosgrigorakis.logisticsplatform.companyprofile.model.CompanyProfile;
 
 public interface CompanyProfileService {
+    CompanyProfile getCompanyProfileEntity();
+
     CompanyProfileResponse getCompanyProfile();
 
     CompanyProfileResponse createCompanyProfile(CompanyProfileCreateRequest request);
