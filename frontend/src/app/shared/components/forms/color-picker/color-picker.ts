fb2c34bd7d6273cc97a11f5e970a0ebde50eb557
Manos Grigorakis <manos.grigorakis03@gmail.com>
2026-06-26 09:22:32 +0300

    refactor: color picker component to accept default HEX color

--- frontend/src/app/shared/components/forms/color-picker/color-picker.ts
+++ frontend/src/app/shared/components/forms/color-picker/color-picker.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, inject, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, inject, Input, OnInit } from '@angular/core';
 import { ChromePickerComponent, ColorPickerControl } from '@iplab/ngx-color-picker';
 import { FormControl } from '@angular/forms';
 
@@ -8,15 +8,22 @@ import { FormControl } from '@angular/forms';
   templateUrl: './color-picker.html',
   styleUrl: './color-picker.css',
 })
-export class ColorPicker {
+export class ColorPicker implements OnInit {
   @Input({ required: true }) label!: string;
   @Input({ required: true }) colorControl!: FormControl;
   @Input({ required: true }) colorValue!: ColorPickerControl;
   @Input() required: boolean = false;
+  @Input() defaultColor?: string;
 
   private elementRef = inject(ElementRef);
 
   public showColorPicker: boolean = false;
+
+  ngOnInit() {
+    if (!this.colorControl.value) {
+      this.colorControl.setValue(this.defaultColor);
+    }
+  }
 
   public togglePicker(): void {
     this.showColorPicker = !this.showColorPicker;
