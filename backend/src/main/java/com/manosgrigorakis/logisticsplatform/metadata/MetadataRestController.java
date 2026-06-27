f0ce01a7e0de164471861817bacf1909e328b518
manos <manos.grigorakis03@gmail.com>
2026-06-27 11:18:15 +0300

    feat: add caching on metadata controller

--- backend/src/main/java/com/manosgrigorakis/logisticsplatform/metadata/MetadataRestController.java
+++ backend/src/main/java/com/manosgrigorakis/logisticsplatform/metadata/MetadataRestController.java
@@ -12,6 +12,7 @@ import com.manosgrigorakis.logisticsplatform.suppliers.model.enums.SupplierPayme
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,7 @@ public class MetadataRestController {
 
     @Operation(summary = "ENUM Customer Types", description = "List of customer types")
     @ApiResponse(responseCode = "200", description = "List of customer types")
+    @Cacheable(value = "metadata", key = "'customer-types'")
     @GetMapping("/customer-types")
     public ApiResponseWrapper<List<String>> getCustomerTypes() {
         return new ApiResponseWrapper<>(Arrays.stream(CustomerType.values()).map(Enum::name).toList());
@@ -33,6 +35,7 @@ public class MetadataRestController {
 
     @Operation(summary = "ENUM Quote Status", description = "List of quote status")
     @ApiResponse(responseCode = "200", description = "List of quote status")
+    @Cacheable(value = "metadata", key = "'quote-statuses'")
     @GetMapping("/quote-statuses")
     public ApiResponseWrapper<List<String>> getQuoteStatus() {
         return new ApiResponseWrapper<>(Arrays.stream(QuoteStatus.values()).map(Enum::name).toList());
@@ -40,6 +43,7 @@ public class MetadataRestController {
 
     @Operation(summary = "ENUM Quote Item Units", description = "List of quote items units")
     @ApiResponse(responseCode = "200", description = "List of quote items units")
+    @Cacheable(value = "metadata", key = "'quote-item-units'")
     @GetMapping("/quote-item-units")
     public ApiResponseWrapper<List<String>> getQuoteItemUnits() {
         return new ApiResponseWrapper<>(Arrays.stream(QuoteItemUnit.values()).map(Enum::name).toList());
@@ -47,6 +51,7 @@ public class MetadataRestController {
 
     @Operation(summary = "ENUM Shipment Statuses", description = "List of shipment statuses")
     @ApiResponse(responseCode = "200", description = "List of shipment statuses")
+    @Cacheable(value = "metadata", key = "'shipment-statuses'")
     @GetMapping("/shipment-statuses")
     public ApiResponseWrapper<List<ShipmentStatusResponse>> getShipmentStatuses() {
         return new ApiResponseWrapper<>(Arrays.stream(ShipmentStatus.values()).map(
@@ -56,6 +61,7 @@ public class MetadataRestController {
 
     @Operation(summary = "ENUM Shipment Cargo Unit", description = "List of shipment cargo units")
     @ApiResponse(responseCode = "200", description = "List of shipment cargo units")
+    @Cacheable(value = "metadata", key = "'shipment-cargo-units'")
     @GetMapping("/shipment-cargo-units")
     public ApiResponseWrapper<List<String>> getShipmentCargosUnits() {
         return new ApiResponseWrapper<>(Arrays.stream(ShipmentCargoUnit.values()).map(Enum::name).toList());
@@ -63,6 +69,7 @@ public class MetadataRestController {
 
     @Operation(summary = "ENUM Supplier Payments Status", description = "List of supplier payments statuses")
     @ApiResponse(responseCode = "200", description = "List of supplier payment statuses")
+    @Cacheable(value = "metadata", key = "'supplier-payment-statuses'")
     @GetMapping("/supplier-payments-statuses")
     public ApiResponseWrapper<List<String>> getSupplierPaymentStatuses() {
         return new ApiResponseWrapper<>(Arrays.stream(SupplierPaymentStatus.values()).map(Enum::name).toList());
@@ -70,6 +77,7 @@ public class MetadataRestController {
 
     @Operation(summary = "ENUM Supplier Payments Types", description = "List of supplier payments types")
     @ApiResponse(responseCode = "200", description = "List of supplier payments types")
+    @Cacheable(value = "metadata", key = "'supplier-payment-types'")
     @GetMapping("/supplier-payments-types")
     public ApiResponseWrapper<List<String>> getSupplierPaymentTypes() {
         return new ApiResponseWrapper<>(Arrays.stream(SupplierPaymentType.values()).map(Enum::name).toList());
