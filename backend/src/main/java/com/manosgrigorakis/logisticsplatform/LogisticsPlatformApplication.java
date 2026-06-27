ff1ba4a79920bfb7ca8f3e030ffe76a79151b7c6
manos <manos.grigorakis03@gmail.com>
2026-06-27 14:52:37 +0300

    feat: enable pagination VIA_DTO in backend

--- backend/src/main/java/com/manosgrigorakis/logisticsplatform/LogisticsPlatformApplication.java
+++ backend/src/main/java/com/manosgrigorakis/logisticsplatform/LogisticsPlatformApplication.java
@@ -3,8 +3,10 @@ package com.manosgrigorakis.logisticsplatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 public class LogisticsPlatformApplication {
 
