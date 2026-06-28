ce511ccc46e2a9277360be4856f08e3a9145afeb
Manos Grigorakis <manos.grigorakis03@gmail.com>
2026-06-28 21:04:23 +0300

    Feat/redoc api docs (#199)
    * feat: add redoc page with Open API docs
    
    * docs: add docs on Analytics controller
    
    * fix: update api docs on redoc

--- backend/src/main/java/com/manosgrigorakis/logisticsplatform/analytics/controller/AnalyticsController.java
+++ backend/src/main/java/com/manosgrigorakis/logisticsplatform/analytics/controller/AnalyticsController.java
@@ -7,6 +7,9 @@ import com.manosgrigorakis.logisticsplatform.common.dto.ValueResponse;
 import com.manosgrigorakis.logisticsplatform.payments.enums.InvoiceStatus;
 import com.manosgrigorakis.logisticsplatform.quotes.enums.QuoteStatus;
 import com.manosgrigorakis.logisticsplatform.shipments.enums.ShipmentStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,42 +18,62 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.util.List;
 
+@Tag(name = "Analytics", description = "Dashboard metrics and aggregated business insights")
 @RequiredArgsConstructor
 @RequestMapping("${app.api.prefix}/v1/analytics")
 @RestController
 public class AnalyticsController {
     private final AnalyticsService analyticsService;
 
+    @Operation(summary = "Get Total Customers", description = "Returns total number of registered customers")
+    @ApiResponse(responseCode = "200", description = "Count of customers")
     @GetMapping("/total-customers")
     public ApiResponseWrapper<ValueResponse<Long>> getTotalCustomers(){
         return new ApiResponseWrapper<>(analyticsService.getTotalCustomers());
     }
 
+    @Operation(summary = "Get Total Shipments", description = "Returns total number of shipments")
+    @ApiResponse(responseCode = "200", description = "Count of shipments")
     @GetMapping("/total-shipments")
     public ApiResponseWrapper<ValueResponse<Long>> getTotalShipments(){
         return new ApiResponseWrapper<>(analyticsService.getTotalShipments());
     }
 
+    @Operation(summary = "Get Total Outstanding Invoices Amount", description =
+            "Returns the total remaining unpaid amount for invoices marked as OUTSTANDING or PARTIALLY_PAID")
+    @ApiResponse(responseCode = "200", description = "Total outstanding invoices amount")
     @GetMapping("/total-outstanding-amount")
     public ApiResponseWrapper<ValueResponse<BigDecimal>> getTotalOutstandingInvoicesAmount(){
         return new ApiResponseWrapper<>(analyticsService.getTotalOutstandingInvoicesAmount());
     }
 
+    @Operation(summary = "Get Upcoming Pending Shipments Count",
+            description = "Returns the total number of pending shipments scheduled until tomorrow")
+    @ApiResponse(responseCode = "200", description = "Upcoming pending shipments count")
     @GetMapping("/total-pending-shipments")
     public ApiResponseWrapper<ValueResponse<Integer>> getUpcomingPendingShipments(){
         return new ApiResponseWrapper<>(analyticsService.getUpcomingPendingShipments());
     }
 
+    @Operation(summary = "Get Quotes Grouped by Status",
+            description = "Returns aggregated quotes statistics grouped by status")
+    @ApiResponse(responseCode = "200", description = "Found quotes grouped by status")
     @GetMapping("/quotes-by-status")
     public ApiResponseWrapper<List<ValueByStatus<QuoteStatus>>> getQuotesByStatus(){
         return new ApiResponseWrapper<>(analyticsService.getQuotesByStatus());
     }
 
+    @Operation(summary = "Get Shipments Grouped by Status",
+            description = "Returns aggregated shipments statistics grouped by status")
+    @ApiResponse(responseCode = "200", description = "Found shipments grouped by status")
     @GetMapping("/shipments-by-status")
     public ApiResponseWrapper<List<ValueByStatus<ShipmentStatus>>> getShipmentsByStatus(){
         return new ApiResponseWrapper<>(analyticsService.getShipmentsByStatus());
     }
 
+    @Operation(summary = "Get Invoices Grouped by Status",
+            description = "Returns aggregated invoices statistics grouped by status")
+    @ApiResponse(responseCode = "200", description = "Found invoices grouped by status")
     @GetMapping("/invoices-by-status")
     public ApiResponseWrapper<List<ValueByStatus<InvoiceStatus>>> getInvoicesByStatus(){
         return new ApiResponseWrapper<>(analyticsService.getInvoicesByStatus());
